b5ae0aee8b594db685dcc59785a7ab318f332af5
Shalu6bajpai <bajpaishalu6@gmail.com>
2020-09-10 23:25:50 -0700

    added view engine

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ const app=express();
 const port=8000;
 //Use express router
 app.use('/',require('./routes'));
+
+
+//setup the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
+
+
+
+
+
 app.listen(port,function(err){
     if(err){
         console.log("error",err);
